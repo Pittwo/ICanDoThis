9182a0beb5787de1703163911552b997e5e6b518
Pittwo <malcolm.molloy@stu.ocsb.ca>
2022-12-02 14:07:12 -0500

    done

--- src/Icandothis/Subprograms.java
+++ src/Icandothis/Subprograms.java
@@ -10,7 +10,17 @@ public class Subprograms {
 		System.out.println("input first string: ");
 		String FirstString = in.next();
 		System.out.println("input second string: ");
+		String SecString = in.next();
+		int StringLength = Math.max(FirstString.length(), SecString.length());
 		
+		for (int i = 0; i<StringLength; i++) {
+			if (i<FirstString.length()) {
+				System.out.print(FirstString.charAt(i));
+			}
+			if (i<SecString.length()) {
+				System.out.print(SecString.charAt(i));
+			}
+		}
 	}
 
 }
